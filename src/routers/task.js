6ccbe43c46dadb20d49c3067030d7184ad3cfea9
Lucas Mazzoni <hellolucasm@gmail.com>
2022-12-08 15:22:31 +0100

    Setup filtering and pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,10 +21,22 @@ router.post("/task", auth, async(req, res)=>{
 })
 
 router.get("/tasks", auth, async(req, res)=>{
-    
+    const match = {}; 
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'; 
+    }
+
     try {
-        const taskFound = await Task.find({owner: req.user._id}); 
-        res.send(taskFound); 
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options:{
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
+        })
+        res.send(req.user.tasks); 
     } catch (e) {
         res.status(500).send(e); 
     }
