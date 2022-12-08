0d76bc9000a83d3b4645ee970028e8d06dc1ab12
Lucas Mazzoni <hellolucasm@gmail.com>
2022-12-08 15:47:38 +0100

    Sorting tasks setup

--- src/routers/task.js
+++ src/routers/task.js
@@ -27,13 +27,22 @@ router.get("/tasks", auth, async(req, res)=>{
         match.completed = req.query.completed === 'true'; 
     }
 
+    const sort = {}
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':'); 
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1; 
+    }
+
+
     try {
         await req.user.populate({
             path: 'tasks',
             match,
             options:{
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         res.send(req.user.tasks); 
